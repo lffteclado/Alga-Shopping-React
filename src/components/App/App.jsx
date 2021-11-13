cd2b862bc92bc842c89fe15fe0539cb781cc53c3
Luis Felipe <lffteclado@gmail.com>
2021-11-12 21:41:27 -0300

    Lista de Produtos

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,53 @@
-import React, { useEffect, useState } from "react";
-import Checkbox from "../../shared/Checkbox";
+import React, { useState, useEffect } from "react";
 import LineChart from "../../shared/LineChart";
 import AppContainer from "../AppContainer";
 import AppHeader from '../AppHeader'
 import { Container, Wrapper } from "./App.style";
+import ShoppingList from "../ShoppingList";
+import productMock from '../../mocks/products.json'
 
 export default function App (){
-const [lettuce, setLettuce ] = useState(true)
-const [rice, setRice] = useState(false)
-const [healthy, setHealthy] = useState(20);
-
 const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
 
+const [products, setProducts] = useState(productMock.products)
+const [selectedProducts, setSelectedProducts] = useState([])
+
 useEffect(() =>{
+  const newSelectedProducts = products
+    .filter(product => product.checked)
+  setSelectedProducts(newSelectedProducts)
+}, [products])
+
+function handleToggle(id){
+  const newProduct = products.map(product =>
+    product.id === id ? {...product, checked: !product.checked} : product
+  )
+
+  setProducts(newProduct)
+}
+
+/*useEffect(() =>{
   setTimeout(() => {
     setHealthy(80)
   }, 5000)
-},[])
+},[])*/
 
    return <Wrapper> 
     <Container>
         <AppHeader />
         <AppContainer
          left={
-          <div>
-            Produtos Disponíveis
-            <Checkbox 
-              value={lettuce}
-              title="Alface"
-              onClick={() => setLettuce(!lettuce)}
-            />
-            <Checkbox 
-              value={rice}
-              title="Arroz"
-              onClick={() => setRice(!rice)}
-            />
-          </div>}
+            <ShoppingList
+               title="Produtos Disponíveis"
+                 products={products}
+                 onToggle={handleToggle}
+               />}
           middle={
-            <div>
-                Lista de Compras
-            </div>
-          }
+            <ShoppingList
+               title="Sua Lista de Compras" 
+               products={selectedProducts}
+               onToggle={handleToggle}
+               />}
           right={
               <div>
                 Estatisticas
---
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Wrapper, Title, Array } from "./ShoppingList.styles";
+import Checkbox from '../../shared/Checkbox'
+
+export default function ShoppingList ({ title , products, onToggle}) {
+    return <Wrapper>
+        <Title>
+            { title }:
+        </Title>
+        <Array>            
+            {
+                products.map((product)=>
+                 <Checkbox
+                  value={product.checked}
+                  title={product.name}
+                  onClick={() => onToggle(product.id)}
+                   />)
+            }
+        </Array>
+    </Wrapper>
+}
